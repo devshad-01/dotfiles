dcca1a30668d3feaa992bb4196302b64826f44cb
Shadrack Mutinda <mutinda.shadrack20@gmail.com>
2025-12-29 12:23:05 +0300

    Add uao script for unzipping and opening build files in IntelliJ

--- bin/touchpad-fix.sh
+++ bin/touchpad-fix.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Enable tap to click
-xinput set-prop "AlpsPS/2 ALPS GlidePoint" "libinput Tapping Enabled" 1
+xinput set-prop "AlpsPS/2 ALPS GlidePoint" "Synaptics Tap Action" 2 3 0 0 1 3 2
 
-# Enable two-finger tap for right click
-xinput set-prop "AlpsPS/2 ALPS GlidePoint" "libinput Tapping Button Mapping Enabled" 1 0
+# Enable two-finger tap for right click (already set)
+# xinput set-prop "AlpsPS/2 ALPS GlidePoint" "Synaptics Tap Action" 2 3 0 0 1 3 2
 
-# Enable natural scrolling (optional - comment out if you don't like it)
-# xinput set-prop "AlpsPS/2 ALPS GlidePoint" "libinput Natural Scrolling Enabled" 1
+# Set normal scrolling (positive values for vertical)
+xinput set-prop "AlpsPS/2 ALPS GlidePoint" "Synaptics Scrolling Distance" 76 76
 
 # Increase cursor speed
-xinput set-prop "AlpsPS/2 ALPS GlidePoint" "libinput Accel Speed" 0.5
+xinput set-prop "AlpsPS/2 ALPS GlidePoint" "Synaptics Move Speed" 1.0 1.75 0.052056 0.0
---
+++ bin/uao.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+void main(String[] args) throws Exception {
+    if (args.length == 0) {
+        System.err.println("Usage: uao <zip-file>");
+        return;
+    }
+
+    var zip = new File(args[0]).getAbsoluteFile();
+
+    if (!zip.exists()) {
+        System.err.println("File not found: " + zip);
+        return;
+    }
+
+    var dir = new File(
+        zip.getAbsolutePath().substring(0, zip.getAbsolutePath().lastIndexOf('.'))
+    );
+
+    // unzip
+    new ProcessBuilder("unzip", "-o", zip.getAbsolutePath())
+        .inheritIO()
+        .start()
+        .waitFor();
+
+    // detect build file and open in IntelliJ
+    for (var build : List.of("pom.xml", "build.gradle", "build.gradle.kts")) {
+        var f = new File(dir, build);
+        if (f.exists()) {
+            new ProcessBuilder("idea", f.getAbsolutePath())
+                .inheritIO()
+                .start();
+            return;
+        }
+    }
+
+    System.err.println("No build file found in " + dir);
+}
