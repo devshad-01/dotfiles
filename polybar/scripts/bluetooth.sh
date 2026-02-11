f3218f1513471f7f9a01fc2c15d947a917d767aa
Shadrack Mutinda <mutinda.shadrack20@gmail.com>
2026-02-12 02:55:22 +0300

    Update polybar config: FontAwesome icons, larger fonts; Add gologin wrapper

--- polybar/scripts/bluetooth.sh
+++ polybar/scripts/bluetooth.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
-# Polybar Bluetooth status script
-
 if bluetoothctl show 2>/dev/null | grep -q "Powered: yes"; then
-    # Bluetooth is on
     if bluetoothctl devices Connected 2>/dev/null | grep -q "Device"; then
-        echo "%{F#89b4fa}BT%{F-} ON"
+        echo "%{F#89b4fa}%{F-}"
     else
-        echo "%{F#6c7086}BT%{F-} OFF"
+        echo "%{F#6c7086}%{F-}"
     fi
 else
-    # Bluetooth is off or not available
-    echo "%{F#6c7086}BT%{F-}"
+    echo "%{F#6c7086}%{F-}"
 fi
--- polybar/scripts/brightness.sh
+++ polybar/scripts/brightness.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-# Polybar brightness script
-
 if brightnessctl get &>/dev/null; then
     brightness=$(brightnessctl get)
     max=$(brightnessctl max)
     percent=$((brightness * 100 / max))
-    echo "%{F#f9e2af}BRI%{F-} $percent%"
+    echo "%{F#f9e2af}%{F-} $percent%"
 else
-    echo "%{F#f9e2af}BRI%{F-} N/A"
+    echo "%{F#f9e2af}%{F-} N/A"
 fi
